70d2b6755db6ac6c6be58859f4a3a72d484fb564
faisal12212092 <163728250+faisal12212092@users.noreply.github.com>
2025-07-22 08:00:22 +0530

    Update authorization.js

--- src/components/authorization.js
+++ src/components/authorization.js
@@ -17,7 +17,7 @@ const AuthPage = () => {
       ? { email: credentials.email, password: credentials.password }
       : credentials;
 
-    const response = await fetch(`http://localhost:5000/api/auth/${endpoint}`, {
+    const response = await fetch(`https://notebookbackend-sfa8.onrender.com/api/auth/${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
